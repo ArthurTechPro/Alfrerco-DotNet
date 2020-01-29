f9eb55037762b7bf19d1559a7d5e289d08e64043
roblthegreat <roblthegreat@users.noreply.github.com>
2020-01-29 09:50:11 -0600

    Fixed potential memory leaks

--- Arkivujo.Alfresco.Search/Extensions.cs
+++ Arkivujo.Alfresco.Search/Extensions.cs
@@ -11,14 +11,15 @@ namespace Arkivujo.Alfresco.Search
         {
             var buffer = new byte[16 * 1024];
 
-            var ms = new MemoryStream();
+            using (var ms = new MemoryStream())
+            {
+                int read;
 
-            int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                    ms.Write(buffer, 0, read);
 
-            while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
-                ms.Write(buffer, 0, read);
-
-            return ms.ToArray();
+                return ms.ToArray();
+            }
         }
     }
 }
